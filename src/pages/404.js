c701cc22979d28a20f2cb468f5d7a774102c385e
David Li <david.minxiao.li@gmail.com>
2019-01-07 00:33:38 -0500

    add about as main page

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="Not found" />
     <h1>Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&#39;t exist... unfortunate.</p>
     <Link to="/">Let&#39;s go home.</Link>
   </Layout>
 );
---
+++ src/pages/work.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+const WorkIndex = () => (
+  <Layout>
+    <SEO title="Work" keywords={[`work`, `gatsby`, `javascript`, `react`]} />
+    <h1>Hey, I'm David</h1>
+    <p>
+      I am a software engineer currently at{' '}
+      <a href="https://quartermaster.com">Quartermaster</a>.
+    </p>
+    <p>
+      I graduated <a href="https://mcgill.ca">McGill University</a> with a
+      degree in Honours Biochemistry. I had initially planned on pursuing
+      medicine - but I found that my passions lie with software.
+    </p>
+  </Layout>
+);
+
+export default WorkIndex;
